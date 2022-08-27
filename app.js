52cef125e57dec8c3c01d8ed0658b4b2e6569d97
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-08-27 22:55:39 +0100

    completed

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 let btn = document.querySelectorAll('.calculator-frame button')
 let input = document.querySelector('.calculator-frame input')
-let clear = document.querySelectorAll('.calculator-frame .clear')
 let del = document.querySelector('.calculator-frame .del')
 let equal = document.querySelector('.calculator-frame .equal')
 let nav = document.querySelector('.nav-links')
@@ -14,7 +13,7 @@ let toggler = document.querySelector('.fa-solid')
 for (let button of btn) {
     button.addEventListener('click', function(e) {
         switch(e.target.textContent) {
-            case 'C':
+            case 'C', 'Stop':
                 input.value = '';
                 break;
 
