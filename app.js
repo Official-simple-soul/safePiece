f7ed3b1b1128e3a1ef9aacdbc2adf299ef0f45b8
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-08-28 15:36:36 +0100

    new me

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let btn = document.querySelectorAll('.calculator-frame button')
 let input = document.querySelector('.calculator-frame input')
@@ -8,6 +8,11 @@ let del = document.querySelector('.calculator-frame .del')
 let equal = document.querySelector('.calculator-frame .equal')
 let nav = document.querySelector('.nav-links')
 let toggler = document.querySelector('.fa-solid')
+let cardOne = document.querySelector('.card-one')
+let cardTwo = document.querySelector('.card-two')
+let random = document.querySelector('.count-random')
+let playerCountOne = document.querySelector('.player-one-count')
+let playerCountTwo = document.querySelector('.player-two-count')
 
 
 for (let button of btn) {
@@ -46,5 +51,29 @@ for (let button of btn) {
 toggler.addEventListener('click', function() {
     nav.classList.toggle('show')
     toggler.classList.toggle('fa-xmark')
+})
+playerCountOne.innerHTML = ''
+
+random.addEventListener('click', function() {
+
+    let randomNumber = Math.floor(Math.random() * num.length)
+    cardOne.innerHTML = randomNumber
+
+        if(cardOne.innerHTML <= 4) {
+            cardOne.style.backgroundColor = 'red'
+        }
+
+        else if(cardOne.innerHTML <= 7) {
+            cardOne.style.backgroundColor = 'purple'
+        }
+
+        else {
+            cardOne.style.backgroundColor = 'black'
+        }
+
+        let firstCount = 0
+        firstCount = firstCount + randomNumber;
+    playerCountOne.innerText = firstCount 
+    // playerCountTwo.innerText = cardTwo.innerHTML;
 })
 
